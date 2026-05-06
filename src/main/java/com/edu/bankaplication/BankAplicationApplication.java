18053469f9b47af55c2bf0b99a0813b2b21c2e22
MartyniukYurii <145149346+masonabor@users.noreply.github.com>
2026-05-06 14:49:29 +0300

    init

---
+++ src/main/java/com/edu/bankaplication/BankAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.edu.bankaplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAplicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/bankaplication/BankAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.bankaplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
